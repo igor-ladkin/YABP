af7a650965dc4521a192bd32a24adb740dd15b23
Igor Ladkin <dgkplan@googlemail.com>
2016-12-21 14:09:58 +0900

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "extends": "airbnb",
+  "installedESLint": true,
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "no-useless-escape": 1,
+    "import/no-named-default": 0
+  }
+};
---
+++ README.md
@@ -0,0 +1,1 @@
+Yet Another Blogging Platform built with ReactJS.
