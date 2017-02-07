046b7e9346fe3bc7cbca2c8cf39765c30262349d
Igor Ladkin <dgkplan@googlemail.com>
2017-02-07 15:59:36 +0900

    Update linter configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,34 @@
 module.exports = {
-    "extends": "airbnb",
-    "installedESLint": true,
-    "plugins": [
-        "react"
-    ]
+  "env": {
+    "browser": true,
+    "es6": true,
+    "node": true,
+    "jest": true,
+  },
+  "extends": [
+    "airbnb",
+    "plugin:destructuring/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+  ],
+  "installedESLint": true,
+  "plugins": [
+    "react",
+    "destructuring",
+  ],
+  "rules": {
+    "react/jsx-uses-vars": 2,
+    "react/jsx-uses-react": 2,
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+  },
+  "settings": {
+    "import/resolver": {
+      "node": {
+        "moduleDirectory": [
+          "node_modules",
+          "src",
+        ],
+      },
+    },
+  },
 };
