55c46f98e99ae2b05c5a8521c19ec6234e92abd1
Igor Ladkin <dgkplan@googlemail.com>
2017-02-08 15:51:05 +0900

    Delete unused components
    Add some styling.

--- src/components/common/TextBox.jsx
+++
@@ -1,9 +0,0 @@
-import { DOM, PropTypes as PT } from 'react';
-
-const TextBox = props => DOM.span(null, props.children);
-
-TextBox.propsTypes = {
-  children: PT.string.isRequired,
-};
-
-export { TextBox };
--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,3 +1,1 @@
-export * from './Image';
-export * from './TextBox';
 export * from './MetaInfo';
