4d2ea569d407636dc2a0e56819fbf5886d9d94f7
Igor Ladkin <dgkplan@googlemail.com>
2017-03-01 17:54:00 +0900

    Update post actions creators to use API middleware
    Remove redux thunk.

--- src/actions/Post.js
+++ src/actions/Post.js
@@ -1,36 +1,18 @@
-import request from 'superagent';
-
 import * as types from 'constants/actionTypes/Post';
-import { API_ROOT } from 'constants';
 import { postPath } from 'helpers/routes';
-
-function requestPost() {
-  return {
-    type: types.FETCH_POST_REQUEST,
-  };
-}
-
-function errorPost() {
-  return {
-    type: types.FETCH_POST_ERROR,
-  };
-}
-
-function receivePost(payload) {
-  return {
-    type: types.FETCH_POST_SUCCESS,
-    payload,
-  };
-}
+import { API_CALL } from 'middleware/API';
 
 export function fetchPost(id) {
-  return (dispatch) => {
-    dispatch(requestPost());
-
-    return request
-      .get(`${API_ROOT}${postPath(id)}`)
-      .end((err, res) => (
-        err ? dispatch(errorPost()) : dispatch(receivePost(res.body))
-      ));
+  return {
+    [API_CALL]: {
+      endpoint: `${postPath(id)}`,
+      method: 'GET',
+      query: {},
+      types: [
+        types.FETCH_POST_REQUEST,
+        types.FETCH_POST_SUCCESS,
+        types.FETCH_POST_ERROR,
+      ],
+    },
   };
 }
