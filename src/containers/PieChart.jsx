d4c84a853bb8d00d2e07f72d5a85b2bfde01d627
Igor Ladkin <dgkplan@googlemail.com>
2017-03-03 11:45:15 +0900

    Remove unnecessary temporary component when creating PieChart container

--- src/containers/PieChart.jsx
+++ src/containers/PieChart.jsx
@@ -1,29 +1,10 @@
-import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { chain } from 'lodash';
 
 import PieChart from 'components/PieChart';
+import { filteredChartItems } from 'helpers/selectors';
 
-const PieChartContainer = ({ items, handleChartClose }) => {
-  const chartItems =
-    chain(items)
-      .filter(({ meta }) => Number(meta.likeCount) > 0)
-      .map(({ title, meta }) => {
-        const name = title.split('. ')[1].replace('.', '');
-        return [name, Number(meta.likeCount)];
-      }).value();
+const stateToProps = state => ({
+  items: filteredChartItems(state),
+});
 
-  return <PieChart items={chartItems} handleChartClose={handleChartClose} />;
-};
-
-PieChartContainer.propTypes = {
-  items: PropTypes.array.isRequired,
-  handleChartClose: PropTypes.func.isRequired,
-};
-
-const stateToProps = (state) => {
-  const { items } = state.posts;
-  return { items };
-};
-
-export default connect(stateToProps)(PieChartContainer);
+export default connect(stateToProps)(PieChart);
