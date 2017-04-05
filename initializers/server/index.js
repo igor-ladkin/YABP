e7d1a5586b1e4334220ad88e76437084845806bd
Igor Ladkin <dgkplan@googlemail.com>
2017-04-05 15:49:08 +0900

    Setup server boilerplate

--- initializers/server/index.js
+++ initializers/server/index.js
@@ -1,4 +1,12 @@
 /* eslint-disable */
+const path = require('path');
+
+require('app-module-path').addPath(path.join(process.cwd(), 'src'));
+require('./globals');
+require('babel-core/register');
+require.extensions['.css'] = () => {
+  return;
+}
 
 const webpack = require('webpack');
 const webpackDevServer = require('webpack-dev-server');
@@ -8,17 +16,16 @@ const config = require('../../webpack.config.js');
 const host = 'localhost';
 const port = 3000;
 
-new webpackDevServer(webpack(config), {
-  hot: true,
-  historyApiFallback: true,
-  publicPath: config.output.publicPath,
-  stats: {
-    colors: true,
-  },
-}).listen(port, host, err => {
-  if (err) {
-    console.log(err);
-  }
+const express = require('express');
+const application = express();
 
+application.use(express.static('src/static'));
+
+application.set('views', __dirname);
+application.set('view engine', 'ejs');
+
+application.get('*', require('./render').default);
+
+application.listen(port, function() {
   console.log(`Listening at host: ${host} port: ${port}`);
 });
